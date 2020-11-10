b76d45500c6e6c6e6dbab1ffc4f5cf86495659d4
AaronKable <aaronkable@gmail.com>
2020-11-10 10:28:30 +0800

    add outstanding

--- invoices/app_settings.py
+++ invoices/app_settings.py
@@ -1,4 +1,15 @@
 from django.conf import settings
+import re
+
+def get_site_url():  # regex sso url
+    regex = r"^(.+)\/s.+"
+    matches = re.finditer(regex, settings.ESI_SSO_CALLBACK_URL, re.MULTILINE)
+    url = "http://"
+
+    for m in matches:
+        url = m.groups()[0] # first match
+
+    return url
 
 PAYMENT_CORP = getattr(settings, "PAYMENT_CORP", 1639878825)
 
